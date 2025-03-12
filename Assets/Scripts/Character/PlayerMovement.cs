b467d20ec56909147a067c3aae05b4c7909d7e9f
MSI\luisi <22295673@live.uem.es>
2025-03-12 13:53:30 +0100

    Movimiento de tanque y bases de apuntado

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 3.5f;    // Velocidad de avance/retroceso
+    public float rotationSpeed = 150f; // Velocidad de rotación en grados por segundo
+    private CharacterController controller;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Movimiento adelante / atrás
+        float moveDirection = Input.GetAxis("Vertical"); // W (1) / S (-1)
+        Vector3 move = transform.forward * moveDirection * moveSpeed * Time.deltaTime;
+        controller.Move(move);
+
+        // Rotación izquierda / derecha
+        float rotation = Input.GetAxis("Horizontal"); // A (-1) / D (1)
+        transform.Rotate(Vector3.up * rotation * rotationSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Character/PlayerShoot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject crosshairUI; // Referencia al crosshair en la UI
+    //public Animator animator; // Referencia al Animator
+    public GameObject bulletPrefab;
+    public Transform firePoint; // Lugar desde donde dispara
+    public float bulletSpeed = 20f;
+
+    private bool isAiming = false;
+
+    void Update()
+    {
+        // Activar modo de apuntado con Click Derecho (Botón Secundario)
+        if (Input.GetMouseButton(1)) // Click derecho
+        {
+            isAiming = true;
+            crosshairUI.SetActive(true); // Mostrar crosshair
+            //animator.SetBool("IsAiming", true); // Activar animación de apuntado
+        }
+        else
+        {
+            isAiming = false;
+            crosshairUI.SetActive(false); // Ocultar crosshair
+            //animator.SetBool("IsAiming", false); // Volver a animación normal
+        }
+
+        // Disparar con Click Izquierdo
+        if (isAiming && Input.mouseScrollDelta.y != 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.linearVelocity = firePoint.forward * bulletSpeed;
+
+        //animator.SetTrigger("Shoot"); // Activar animación de disparo
+    }
+}
